1aa71744cd2c81e70d711fe3e81a453b14679ad1
Kristhian David Segura <kristhian.segura@mail.escuelaing.edu.co>
2022-02-08 19:19:39 -0500

    Procfile add

--- src/main/java/edu/escuelaing/arep/app/SparkApp.java
+++ src/main/java/edu/escuelaing/arep/app/SparkApp.java
@@ -6,7 +6,7 @@ public class SparkApp {
 
     public static void main(String[] args) {
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroku");
+        get("/hello", (req, res) -> "Hello World");
     }
 
     static int getPort() {
