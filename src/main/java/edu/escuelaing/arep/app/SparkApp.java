e6cbeff659431b1585b4af0bb126351bc1e02dbe
Kristhian David Segura <kristhian.segura@mail.escuelaing.edu.co>
2022-02-08 21:03:54 -0500

    Arreglo ruta index.html

--- src/main/java/edu/escuelaing/arep/app/SparkApp.java
+++ src/main/java/edu/escuelaing/arep/app/SparkApp.java
@@ -4,6 +4,13 @@ import spark.Spark;
 import spark.utils.IOUtils;
 
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static spark.Spark.*;
 
 public class SparkApp {
@@ -11,7 +18,7 @@ public class SparkApp {
     public static void main(String[] args) {
         port(getPort());
         get("/hello", (req, res) -> "Hello World");
-        get("/", (req, res) -> IOUtils.toString(Spark.class.getResourceAsStream("index.html")));
+        get("/", (req, res) -> renderIndex());
     }
 
     static int getPort() {
@@ -22,7 +29,15 @@ public class SparkApp {
         //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 
-
+    private static String renderIndex() {
+        try {
+            URL url = SparkApp.class.getResource("/public/index.html");
+            return new String(Files.readAllBytes(Paths.get(url.toURI())), Charset.defaultCharset());
+        } catch (IOException | URISyntaxException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 
 }
 
