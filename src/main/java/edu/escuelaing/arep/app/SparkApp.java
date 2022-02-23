92694daea860de6b4252cd7eb6c4527e313cb401
Kristhian David Segura <kristhian.segura@mail.escuelaing.edu.co>
2022-02-23 02:12:57 -0500

    Json

--- src/main/java/edu/escuelaing/arep/app/SparkApp.java
+++ src/main/java/edu/escuelaing/arep/app/SparkApp.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import com.google.gson.Gson;
 
 import static spark.Spark.*;
 
@@ -15,8 +16,14 @@ public class SparkApp {
 
     public static void main(String[] args) {
         port(getPort());
-        get("/celsius", (req, res) -> "La conversión de " + req.queryParams("celsius") + "° grados Celsius a grados Fahrenheit es: " + convertToFahrenheit(req.queryParams("celsius")) + "°");
-        get("/fahrenheit", (req, res) -> "La conversión de " + req.queryParams("fahrenheit") + "° grados Fahrenheit a grados Celsius es: " + convertToCelsius(req.queryParams("fahrenheit")) + "°");
+        get("/celsius", (req, res) -> {
+            res.type("application/json");
+            return convertToFahrenheit(req.queryParams("celsius"));
+        });
+        get("/fahrenheit", (req, res) -> {
+            res.type("application/json");
+            return convertToCelsius(req.queryParams("fahrenheit"));
+        });
         get("/", (req, res) -> renderIndex());
     }
 
@@ -39,11 +46,11 @@ public class SparkApp {
     }
 
     private static String convertToCelsius(String gradosFahr){
-        return String.valueOf(new Fahrenheit(Double.parseDouble(gradosFahr)).convertToCelsius());
+        return new Gson().toJson(new Fahrenheit(Double.parseDouble(gradosFahr)).convertToCelsius());
     }
 
     private static String convertToFahrenheit(String gradosCel){
-        return String.valueOf(new Celsius(Double.parseDouble(gradosCel)).convertToFahrenheit());
+        return new Gson().toJson(new Celsius(Double.parseDouble(gradosCel)).convertToFahrenheit());
     }
 
 
